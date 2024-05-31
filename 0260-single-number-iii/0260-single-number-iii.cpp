25699cfd7cb7eab5a589212e346df8eb9f778764
Kunal Tak <115993113+kbash11@users.noreply.github.com>
2024-05-31 23:12:26 +0530

    Time: 3 ms (96.62%), Space: 12.3 MB (64.77%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -1,50 +1,48 @@
-class Solution {
-public:
-    vector<int> singleNumber(vector<int>& nums) {
-        unordered_map<int,int> m;
-        for(auto it:nums){
-            m[it]++;
-            if(m[it]==2){
-                m.erase(it);
-            }
-        }
-        vector<int> ans;
-        for(auto i: m){
-            if(i.second == 1){
-                ans.push_back(i.first);
-            }
-        }
-        return ans;
-    }
-};
-
 // class Solution {
 // public:
 //     vector<int> singleNumber(vector<int>& nums) {
-//         // Step 1: Perform XOR of all numbers to get XOR of the two unique numbers
-//         int XOR = 0;
-//         for (int num : nums) {
-//             XOR ^= num;
-//         }
-
-//         // Step 2: Find the rightmost set bit in XOR
-//         // Instead of using ~ and - operations, we find the rightmost set bit directly
-//         int bitmask = 1;
-//         while ((XOR & bitmask) == 0) {
-//             bitmask <<= 1;
-//         }
-
-//         // Step 3: Divide numbers into two groups based on the bit found and XOR within each group
-//         int a = 0, b = 0;
-//         for (int num : nums) {
-//             if (num & bitmask) {
-//                 a ^= num;
-//             } else {
-//                 b ^= num;
+//         unordered_map<int,int> m;
+//         for(auto it:nums){
+//             m[it]++;
+//             if(m[it]==2){
+//                 m.erase(it);
 //             }
 //         }
-
-//         // Step 4: Return the two unique numbers
-//         return {a, b};
+//         vector<int> ans;
+//         for(auto i: m){
+//             if(i.second == 1){
+//                 ans.push_back(i.first);
+//             }
+//         }
+//         return ans;
 //     }
 // };
+
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        // Step 1: Perform XOR of all numbers to get XOR of the two unique numbers
+        long long XOR = 0;
+        for (int num : nums) {
+            XOR ^= num;
+        }
+
+        // Step 2: Find the rightmost set bit in XOR
+        // Instead of using ~ and - operations, we find the rightmost set bit directly
+        long long bitmask = XOR & (-XOR);
+        
+
+        // Step 3: Divide numbers into two groups based on the bit found and XOR within each group
+        int a = 0, b = 0;
+        for (int num : nums) {
+            if (num & bitmask) {
+                a ^= num;
+            } else {
+                b ^= num;
+            }
+        }
+
+        // Step 4: Return the two unique numbers
+        return {a, b};
+    }
+};
