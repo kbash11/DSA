6605ab9bfbe0be08f4ffeea894afa76cf314d544
Kunal Tak <115993113+kbash11@users.noreply.github.com>
2024-06-10 13:01:36 +0530

    Time: 0 ms (100%), Space: 10.1 MB (78.64%) - LeetHub

--- 1137-height-checker/1137-height-checker.cpp
+++ 1137-height-checker/1137-height-checker.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
     int heightChecker(vector<int>& heights) {
-        priority_queue<int,vector<int>,greater<int>> pq;
-        for(auto it:heights){
-            pq.push(it);
-        }
+        vector<int> arr=heights;
+        sort(arr.begin(),arr.end());
         int count=0;
-        for(int i=0;i<heights.size();i++){
-            if(pq.top()!=heights[i]){
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]!=heights[i]){
                 count++;
             }
-            pq.pop();
         }
         return count;
+        // priority_queue<int,vector<int>,greater<int>> pq;
+        // for(auto it:heights){
+        //     pq.push(it);
+        // }
+        // int count=0;
+        // for(int i=0;i<heights.size();i++){
+        //     if(pq.top()!=heights[i]){
+        //         count++;
+        //     }
+        //     pq.pop();
+        // }
+        // return count;
     }
 };
