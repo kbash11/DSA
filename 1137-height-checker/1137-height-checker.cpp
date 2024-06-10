2d81e78516dc6254d9f3937e0ce75b289b74fb5e
Kunal Tak <115993113+kbash11@users.noreply.github.com>
2024-06-10 13:48:08 +0530

    Time: 6 ms (31.17%), Space: 11.6 MB (10.73%) - LeetHub

--- 1137-height-checker/1137-height-checker.cpp
+++ 1137-height-checker/1137-height-checker.cpp
@@ -1,15 +1,43 @@
 class Solution {
 public:
     int heightChecker(vector<int>& heights) {
-        vector<int> arr=heights;
-        sort(arr.begin(),arr.end());
+
+        unordered_map<int,int> m;
+        int mini=INT_MAX;
+        int maxi=INT_MIN;
+        for(int i=0;i<heights.size();i++){
+            mini=min(mini,heights[i]);
+            maxi=max(maxi,heights[i]);
+            m[heights[i]]++;
+        }
+        int ind=0;
+        vector<int> sortedArr(heights.size());
+        for(int val=mini;val<=maxi;val++){
+            while(m[val]>0){
+                sortedArr[ind]=val;
+                ind++;
+                m[val]--;
+            }
+        }
         int count=0;
-        for(int i=0;i<arr.size();i++){
-            if(arr[i]!=heights[i]){
+        for(int i=0;i<heights.size();i++){
+            if(sortedArr[i]!=heights[i]){
                 count++;
             }
         }
         return count;
+
+        // vector<int> arr=heights;
+        // sort(arr.begin(),arr.end());
+        // int count=0;
+        // for(int i=0;i<arr.size();i++){
+        //     if(arr[i]!=heights[i]){
+        //         count++;
+        //     }
+        // }
+        // return count;
+
+
         // priority_queue<int,vector<int>,greater<int>> pq;
         // for(auto it:heights){
         //     pq.push(it);
