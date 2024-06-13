4a4f7a8c738b50611049873d484e136058394b1e
Kunal Tak <115993113+kbash11@users.noreply.github.com>
2024-06-14 00:33:28 +0530

    Time: 75 ms (84.33%), Space: 47.2 MB (21.38%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.cpp
+++ 0744-network-delay-time/0744-network-delay-time.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<list<pair<int, int>>> adj(n + 1);
-        for (int i = 0; i < times.size(); i++) {
-            adj[times[i][0]].push_back({times[i][1], times[i][2]});
+        vector<vector<pair<int,int>>> adj(n+1,vector<pair<int,int>>{} );
+        for(auto x:times){
+            adj[x[0]].push_back({x[1],x[2]});
         }
 
-        queue<pair<int, int>> q;
-        q.push({0, k});
-        vector<int> dist(n + 1, INT_MAX);
-        dist[k] = 0;
+        vector<int> dist(n+1,1e9);
+        dist[k]=0;
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        pq.push({0,k});
 
-        while (!q.empty()) {
-            auto [time, node] = q.front();
-            q.pop();
+        while(!pq.empty()){
+            int node=pq.top().second;
+            int dis=pq.top().first;
+            pq.pop();
 
-            for (auto it : adj[node]) {
-                if (time + it.second < dist[it.first]) {
-                    dist[it.first] = time + it.second;
-                    q.push({dist[it.first], it.first});
+            for(auto x:adj[node]){
+                int adjNode=x.first;
+                int adjDist=x.second;
+                if(dis+adjDist < dist[adjNode]){
+                    dist[adjNode]=dis+adjDist;
+                    pq.push({adjDist+dis,adjNode});
                 }
             }
         }
-
-        int ans = INT_MIN;
-        for (int i = 1; i <= n; i++) {
-            if (dist[i] == INT_MAX) {
-                return -1;
-            }
-            ans = max(dist[i], ans);
+        int ans=-1;
+        for(int i=1;i<=n;i++){
+            ans=max(ans,dist[i]);
         }
+        if(ans==1e9){return -1;}
         return ans;
     }
 };
