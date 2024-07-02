dadf24d1a6b823993fb8835fbc307392d385a589
Kunal Tak <115993113+kbash11@users.noreply.github.com>
2024-07-02 18:09:32 +0530

    Time: 0 ms (100%), Space: 12.6 MB (59.7%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -1,17 +1,35 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int,int> m;
-        for(auto it:nums1){
-            m[it]++;
-        }
+
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
         vector<int> ans;
-        for(int i=0;i<nums2.size();i++){
-            if(m[nums2[i]]>0){
-                ans.push_back(nums2[i]);
-                m[nums2[i]]--;
+        int i=0,j=0;
+        while(i<nums1.size() && j<nums2.size()){
+            if(nums1[i]<nums2[j]){
+                i++;
+            }else if(nums1[i]>nums2[j]){
+                j++;
+            }else{
+                ans.push_back(nums1[i]);
+                i++;
+                j++;
             }
         }
         return ans;
+
+        // unordered_map<int,int> m;           // T.C-O(M+N)   S.C-O(M)
+        // for(auto it:nums1){
+        //     m[it]++;
+        // }
+        // vector<int> ans;
+        // for(int i=0;i<nums2.size();i++){
+        //     if(m[nums2[i]]>0){
+        //         ans.push_back(nums2[i]);
+        //         m[nums2[i]]--;
+        //     }
+        // }
+        // return ans;
     }
 };
