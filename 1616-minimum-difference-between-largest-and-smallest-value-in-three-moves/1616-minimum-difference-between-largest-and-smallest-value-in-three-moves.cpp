98b32f52d4365a6430def9dee2e9a943c21f3bb5
Kunal Tak <115993113+kbash11@users.noreply.github.com>
2024-07-04 12:39:18 +0530

    Time: 43 ms (97.9%), Space: 37.7 MB (96.91%) - LeetHub

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/1616-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/1616-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -5,7 +5,17 @@ public:
         if(n<=4){
             return 0;
         }
-        sort(nums.begin(),nums.end());
+        // sort(nums.begin(),nums.end());          T.C-O(n)
+
+        //sort starting 4 elements
+        partial_sort(nums.begin(),nums.begin()+4,nums.end());      // O(nlog(4))   ~ O(n)
+
+        //element at (n-4)th pos. if sorted
+        nth_element(nums.begin()+4,nums.begin()+(n-4),nums.end());   //O(n)
+
+        //sort last 4 elements
+        sort(nums.begin()+(n-4),nums.end());                         // O(4log(4))  `O(1)
+
         int ans=INT_MAX;
         ans=min(ans,nums[n-4]-nums[0]);
         ans=min(ans,nums[n-3]-nums[1]);
