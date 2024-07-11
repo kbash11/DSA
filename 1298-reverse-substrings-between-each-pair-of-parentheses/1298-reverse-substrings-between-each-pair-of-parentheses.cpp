fe15bdf1273c07f3ea57f01eb1ffd89ad5d46ea1
Kunal Tak <115993113+kbash11@users.noreply.github.com>
2024-07-11 13:06:51 +0530

    Time: 0 ms (100%), Space: 7.9 MB (49.11%) - LeetHub

--- 1298-reverse-substrings-between-each-pair-of-parentheses/1298-reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/1298-reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -1,35 +1,62 @@
 class Solution {
 public:
     string reverseParentheses(string s) {
-        stack<string> st;
-        int i = 0;
 
-        while (i < s.size()) {
-            if (s[i] != ')') {
-                string str(1, s[i]);
-                st.push(str);
-                i++;
-            } else {
-                string str = "";
-                while (!st.empty() && st.top() != "(") {
-                    str = st.top() + str;
-                    st.pop();
-                }
-                if (!st.empty()) {
-                    st.pop();
-                }
-                reverse(str.begin(), str.end());
-                st.push(str);
-                i++;
+        unordered_map<int, int> m; // To store the index of closing and corresponding opening brackets
+        stack<int> stk;
+
+        // Fill the map with corresponding parentheses indices
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] == '(') {
+                stk.push(i);
+            } else if (s[i] == ')') {
+                int openIndex = stk.top();
+                stk.pop();
+                m[openIndex] = i;
+                m[i] = openIndex;
             }
         }
-
-        string result = "";
-        while (!st.empty()) {
-            result = st.top() + result;
-            st.pop();
+        int flag=1;
+        string ans;
+        for(int i=0;i<s.size();i+=flag){
+            if(s[i]=='(' || s[i]==')'){
+                i=m[i];
+                flag=-flag;
+            }else{
+                ans.push_back(s[i]);
+            }
         }
+        return ans;
 
-        return result;
+        // stack<string> st;                        T.C-O(n)
+        // int i = 0;
+
+        // while (i < s.size()) {
+        //     if (s[i] != ')') {
+        //         string str(1, s[i]);
+        //         st.push(str);
+        //         i++;
+        //     } else {
+        //         string str = "";
+        //         while (!st.empty() && st.top() != "(") {
+        //             str = st.top() + str;
+        //             st.pop();
+        //         }
+        //         if (!st.empty()) {
+        //             st.pop();
+        //         }
+        //         reverse(str.begin(), str.end());
+        //         st.push(str);
+        //         i++;
+        //     }
+        // }
+
+        // string result = "";
+        // while (!st.empty()) {
+        //     result = st.top() + result;
+        //     st.pop();
+        // }
+
+        // return result;
     }
 };
