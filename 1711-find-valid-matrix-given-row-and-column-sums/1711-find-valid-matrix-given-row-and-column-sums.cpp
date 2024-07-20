d1c13793aafe9281b8acad5bb4e123a2830013c1
Kunal Tak <115993113+kbash11@users.noreply.github.com>
2024-07-20 12:42:17 +0530

    Time: 31 ms (88.26%), Space: 35.9 MB (27.54%) - LeetHub

--- 1711-find-valid-matrix-given-row-and-column-sums/1711-find-valid-matrix-given-row-and-column-sums.cpp
+++ 1711-find-valid-matrix-given-row-and-column-sums/1711-find-valid-matrix-given-row-and-column-sums.cpp
@@ -1,14 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> restoreMatrix(vector<int>& rowSum, vector<int>& colSum) {
-        vector<vector<int>> ans(rowSum.size(),vector<int>(colSum.size(),0));
-        for(int i=0;i<rowSum.size();i++){
-            for(int j=0;j<colSum.size();j++){
-                ans[i][j]=min(rowSum[i],colSum[j]);
-                rowSum[i]-=ans[i][j];
-                colSum[j]-=ans[i][j];
+        // vector<vector<int>> ans(rowSum.size(),vector<int>(colSum.size(),0));         // T.C-O(n*m)   S.C-O(1)
+        // for(int i=0;i<rowSum.size();i++){
+        //     for(int j=0;j<colSum.size();j++){
+        //         ans[i][j]=min(rowSum[i],colSum[j]);
+        //         rowSum[i]-=ans[i][j];
+        //         colSum[j]-=ans[i][j];
+        //     }
+        // }
+        // return ans;
+
+        vector<vector<int>> ans(rowSum.size(),vector<int>(colSum.size(),0));    //T.C-O(min(n,m))
+
+        int i=0,j=0;
+        while(i<rowSum.size() && j<colSum.size()){
+            ans[i][j]=min(rowSum[i],colSum[j]);
+            rowSum[i]-=ans[i][j];
+            colSum[j]-=ans[i][j];
+            if(rowSum[i]==0){
+                i++;
+            }else{
+                j++;
             }
         }
+        
         return ans;
     }
 };
