a177ea6a5ed385114ec623010134a6c7a7f7d1c6
Kunal Tak <115993113+kbash11@users.noreply.github.com>
2024-07-30 13:03:55 +0530

    Time: 89 ms (62.36%), Space: 23.7 MB (73.37%) - LeetHub

--- 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.cpp
@@ -2,31 +2,37 @@ class Solution {
 public:
     int minimumDeletions(string s) {
         int n=s.size();
-        stack<char> st;
-        int ans=0;
-        for(int i=0;i<s.size();i++){
-            if(!st.empty() && s[i]=='a' && st.top()=='b'){
-                st.pop();
-                ans++;
+        // stack<char> st;
+        // int ans=0;
+        // for(int i=0;i<s.size();i++){
+        //     if(!st.empty() && s[i]=='a' && st.top()=='b'){
+        //         st.pop();
+        //         ans++;
+        //     }else{
+        //         st.push(s[i]);
+        //     }
+        // }
+        // return ans;
+
+        int counta=0,countb=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='b'){
+                countb++;
             }else{
-                st.push(s[i]);
+                counta++;
+            }
+        }
+        int ans=countb;
+        int leftB=0,rightA=counta;
+        for(int i=0;i<n;i++){
+            if(s[i]=='a'){
+                rightA--;
+            }
+            ans=min(ans,leftB+rightA);
+            if(s[i]=='b'){
+                leftB++;
             }
         }
         return ans;
-
-        // int counta=0,countb=0;
-        // for(int i=0;i<n;i++){
-        //     if(s[i]=='b'){
-        //         countb++;
-        //     }else{
-        //         counta++;
-        //     }
-        // }
-        // int ans=countb;
-        // int preva=0;
-        // for(int i=0;i<n;i++){
-        //     ans=min(ans,)
-        // }
-        // return ans;
     }
 };
