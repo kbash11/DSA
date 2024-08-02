9934f31c8fbfe8171e9e2d0002e6822c216a4819
Kunal Tak <115993113+kbash11@users.noreply.github.com>
2024-08-02 12:57:57 +0530

    Time: 90 ms (5.89%), Space: 83.1 MB (57.56%) - LeetHub

--- 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -7,20 +7,16 @@ public:
                 size++;
             }
         }
-        vector<int> arr=nums;
-        for(int i=0;i<size;i++){
-            arr.push_back(nums[i]);
-        }
-
+        int n=nums.size();
         int i=0,j=0;
         int count0=0,ans=INT_MAX;
-        while(j<arr.size()){
-            if(arr[j]==0){
+        while(j<2*nums.size()){
+            if(nums[j%n]==0){
                 count0++;
             }
             if(j-i+1==size){
                 ans=min(ans,count0);
-                if(arr[i]==0){
+                if(nums[i%n]==0){
                     count0--;
                 }
                 i++;
@@ -31,5 +27,35 @@ public:
             return 0;
         }
         return ans;
+        // int size=0;
+        // for(auto it:nums){
+        //     if(it==1){
+        //         size++;
+        //     }
+        // }
+        // vector<int> arr=nums; // FOR CIRCULAR ARRAY APPEND SIZE ELEMENT AT THE END OF ORIGINAL ARRAY
+        // for(int i=0;i<size;i++){
+        //     arr.push_back(nums[i]);
+        // }
+
+        // int i=0,j=0;
+        // int count0=0,ans=INT_MAX;
+        // while(j<arr.size()){
+        //     if(arr[j]==0){
+        //         count0++;
+        //     }
+        //     if(j-i+1==size){
+        //         ans=min(ans,count0);
+        //         if(arr[i]==0){
+        //             count0--;
+        //         }
+        //         i++;
+        //     }
+        //     j++;
+        // }
+        // if(ans==INT_MAX){
+        //     return 0;
+        // }
+        // return ans;
     }
 };
