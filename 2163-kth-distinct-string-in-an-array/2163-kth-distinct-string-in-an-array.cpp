3437236886dca6b2faa2b275e3d6e63d545a34e1
Kunal Tak <115993113+kbash11@users.noreply.github.com>
2024-08-05 13:24:40 +0530

    Time: 114 ms (5.17%), Space: 15.9 MB (97.75%) - LeetHub

--- 2163-kth-distinct-string-in-an-array/2163-kth-distinct-string-in-an-array.cpp
+++ 2163-kth-distinct-string-in-an-array/2163-kth-distinct-string-in-an-array.cpp
@@ -1,19 +1,28 @@
 class Solution {
 public:
     string kthDistinct(vector<string>& arr, int k) {
-        unordered_map<string,int> m;
-        for(auto it:arr){
-            m[it]++;
-        }
         for(int i=0;i<arr.size();i++){
-            string s=arr[i];
-            if(m[s]==1){
+            if(count(arr.begin(),arr.end(),arr[i])==1){
+                if(k==1){
+                    return arr[i];
+                }
                 k--;
-            }
-            if(k==0){
-                return s;
             }
         }
         return "";
+        // unordered_map<string,int> m;
+        // for(auto it:arr){
+        //     m[it]++;
+        // }
+        // for(int i=0;i<arr.size();i++){
+        //     string s=arr[i];
+        //     if(m[s]==1){
+        //         k--;
+        //     }
+        //     if(k==0){
+        //         return s;
+        //     }
+        // }
+        // return "";
     }
 };
