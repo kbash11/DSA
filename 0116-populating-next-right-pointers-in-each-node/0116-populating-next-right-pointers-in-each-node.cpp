70532df769aa1aab594088fc0233d31c82034ba1
Kunal Tak <115993113+kbash11@users.noreply.github.com>
2024-08-11 20:04:02 +0530

    Time: 20 ms (7.29%), Space: 18.8 MB (46.35%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
@@ -18,40 +18,50 @@ public:
 
 class Solution {
 public:
-
+    void solve(Node* nodeLeft,Node* nodeRight){
+        if(!nodeLeft || !nodeRight){
+            return;
+        }
+        nodeLeft->next=nodeRight;
+        solve(nodeLeft->right,nodeRight->left);
+        solve(nodeLeft->left,nodeLeft->right);
+        solve(nodeRight->left,nodeRight->right);  
+    }
     Node* connect(Node* root) {
         if(!root){
             return NULL;
         }
-        queue<Node*> q;
-        q.push(root);
-        while(!q.empty()){
-            int size=q.size();
-            for(int i=0;i<size;i++){
-                if(i==size-1){
-                    Node* node=q.front();
-                    q.pop();
-                    if(node->left){
-                        q.push(node->left);
-                    }
-                    if(node->right){
-                        q.push(node->right);
-                    }
-                }else{
-                    Node* node=q.front();
-                    q.pop();
-                    if(node->left){
-                        q.push(node->left);
-                    }
-                    if(node->right){
-                        q.push(node->right);
-                    }
-                    Node* node2=q.front();
-                    node->next=node2;
-                    // q.pop();
-                }
-            }
-        }
+        solve(root->left,root->right);
         return root;
+        // queue<Node*> q;
+        // q.push(root);
+        // while(!q.empty()){
+        //     int size=q.size();
+        //     for(int i=0;i<size;i++){
+        //         if(i==size-1){
+        //             Node* node=q.front();
+        //             q.pop();
+        //             if(node->left){
+        //                 q.push(node->left);
+        //             }
+        //             if(node->right){
+        //                 q.push(node->right);
+        //             }
+        //         }else{
+        //             Node* node=q.front();
+        //             q.pop();
+        //             if(node->left){
+        //                 q.push(node->left);
+        //             }
+        //             if(node->right){
+        //                 q.push(node->right);
+        //             }
+        //             Node* node2=q.front();
+        //             node->next=node2;
+        //             // q.pop();
+        //         }
+        //     }
+        // }
+        // return root;
     }
 };
